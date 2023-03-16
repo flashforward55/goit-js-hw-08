8cb9e51771c8b2dcbeb70b786a9d14a7478383c9
Anton Styazhkin <rudana089@gmail.com>
2023-03-16 19:21:14 +0200

    Create elements 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
