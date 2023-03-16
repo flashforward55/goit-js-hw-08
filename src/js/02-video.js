e0fb992a6240ceb162c019b6c20dcae172fb477a
Anton Styazhkin <rudana089@gmail.com>
2023-03-16 18:41:53 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const TIME_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(seconds => {
+      localStorage.setItem(TIME_KEY, seconds);
+    });
+  }, 1000)
+);
+const currentTime = localStorage.getItem(TIME_KEY)
+  ? player.setCurrentTime(localStorage.getItem(TIME_KEY))
+  : 0;
